8ce0ff195814be7adda4dc13bd8f62bb5de25264
Baibhav Kumar <cocc1274@gmail.com>
2024-07-24 23:19:35 +0530

    Update redis.js

--- BE/redis.js
+++ BE/redis.js
@@ -1,14 +1,14 @@
-const { createClient } = require('redis');
-require("dotenv").config()
-const client = createClient({
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    }
-});
-if(!client.isOpen){
-    client.connect()
-    console.log('connected to redis');
-}
-module.exports= {client}
+// const { createClient } = require('redis');
+// require("dotenv").config()
+// const client = createClient({
+//     password: process.env.REDIS_PASSWORD,
+//     socket: {
+//         host: process.env.REDIS_HOST,
+//         port: process.env.REDIS_PORT
+//     }
+// });
+// if(!client.isOpen){
+//     client.connect()
+//     console.log('connected to redis');
+// }
+// module.exports= {client}
